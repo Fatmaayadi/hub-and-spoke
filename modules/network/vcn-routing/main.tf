5feecbb2477bd7ca7484f19e0dfb545ec297a9c3
Fatmaayadi <ayadifatma418@gmail.com>
2025-08-16 13:01:07 +0100

    change

--- modules/network/vcn-routing/main.tf
+++ modules/network/vcn-routing/main.tf
@@ -7,6 +7,7 @@ terraform {
 }
 
 locals {
+  # Génère des tables IGW et SGW avec des clés statiques
   subnets_route_tables_split = merge(
     {
       for name, cfg in var.subnets_route_tables :
@@ -16,7 +17,6 @@ locals {
         compartment_id = cfg.compartment_id
         defined_tags   = cfg.defined_tags
         freeform_tags  = cfg.freeform_tags
-        subnet_id      = cfg.subnet_id
         route_rules    = [for r in cfg.route_rules : r if try(r.destination, null) == "0.0.0.0/0"]
       }
       if anytrue([for r in cfg.route_rules : try(r.destination, null) == "0.0.0.0/0"])
@@ -29,17 +29,11 @@ locals {
         compartment_id = cfg.compartment_id
         defined_tags   = cfg.defined_tags
         freeform_tags  = cfg.freeform_tags
-        subnet_id      = cfg.subnet_id
         route_rules    = [for r in cfg.route_rules : r if try(r.destination, null) == "all-eu-paris-1-services-in-oracle-services-network"]
       }
       if anytrue([for r in cfg.route_rules : try(r.destination, null) == "all-eu-paris-1-services-in-oracle-services-network"])
     }
   )
-
-  subnet_route_table_attachments = {
-    for k, v in local.subnets_route_tables_split :
-    k => v.subnet_id
-  }
 }
 
 resource "oci_core_route_table" "these" {
@@ -61,11 +55,4 @@ resource "oci_core_route_table" "these" {
       description        = rule.description
     }
   }
-}
-
-resource "oci_core_route_table_attachment" "these" {
-  for_each = local.subnet_route_table_attachments
-
-  subnet_id      = each.value
-  route_table_id = oci_core_route_table.these[each.key].id
 }
